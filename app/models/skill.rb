7de71c192a4066029ac5fc685df95a1b0ba9082a
Alton Michaux <alton.michaux@yahoo.com>
2023-06-15 23:56:51 -0400

    building routes and establishing relations

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Skill < ApplicationRecord
+  belongs_to :user
+
   validates :name, presence: true
   validates :description, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,24 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  devise_for :users
+  namespace :api do
+    namespace :v1 do
+      # Devise routes for authentication
+      devise_for :users, path: '', path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      }, controllers: {
+        sessions: 'api/v1/users/sessions',
+        registrations: 'api/v1/users/registrations'
+      }
+
+      resources :users, only: [:index, :show] do
+        resources :skills, only: [:index, :create, :destroy]
+        resources :tickets, only: [:index, :create, :show]
+        resources :matched_skills, only: [:index]
+      end
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
