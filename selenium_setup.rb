3babd11f423fda11997c7be2c6c57d9972c02e94
alton-michaux <alton.michaux@yahoo.com>
2023-06-16 00:06:39 -0400

    setting up chromedriver for testing

---
+++ selenium_setup.rb
@@ -0,0 +1,8 @@
+
+require 'selenium-webdriver'
+
+# Specify the path to chromedriver
+Selenium::WebDriver::Chrome::Service.driver_path = '/usr/bin/chromedriver'
+
+# Create a new instance of the WebDriver using Chrome
+driver = Selenium::WebDriver.for :chrome
