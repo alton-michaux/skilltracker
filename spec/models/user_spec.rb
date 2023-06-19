432383a64ff1c7ff9d46d01d3552adbc161ae563
alton-michaux <alton.michaux@yahoo.com>
2023-06-19 18:04:56 -0400

    user model testing complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,11 @@ RSpec.describe User, type: :model do
     expect(subject).not_to be_valid
   end
 
+  it 'is not valid without a password' do
+    subject.password = nil
+    expect(subject).not_to be_valid
+  end
+
   it 'is not valid with a password shorter than 6 characters' do
     subject.password = 'passw'
     expect(subject).not_to be_valid
