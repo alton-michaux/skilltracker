9db48db615e377548705a3566eaaea09ecdf325d
alton-michaux <alton.michaux@yahoo.com>
2023-06-19 18:09:23 -0400

    skill model covered

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -3,5 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryBot.create(:skill, :javascript) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a description' do
+    subject.description = nil
+    expect(subject).not_to be_valid
+  end
 end
