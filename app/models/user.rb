e075c2ad6004e19558b3c29877444f641e3616b1
alton-michaux <alton.michaux@yahoo.com>
2023-06-19 18:03:24 -0400

    updated user model and model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, length: { in: 6..128 }
+  validates :email, presence: true, format: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/, uniqueness: true
+  validates :first_name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryBot.create(:user, email: 'gina_thompson@email.com', first_name: 'Gina', last_name: 'Thompson') }
+
+  it 'is valid with email, first and last name' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without an email' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it 'is not valid without valid formatted email' do
+    subject.email = 'a@com'
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid with a password shorter than 6 characters' do
+    subject.password = 'passw'
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without first name' do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
 end
