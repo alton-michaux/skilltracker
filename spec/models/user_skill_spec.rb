e86f119d7707ed6c9e50fc7fc8fe7b9fa2fe4fad
alton-michaux <alton.michaux@yahoo.com>
2023-06-19 18:20:48 -0400

    user skill model now has coverage

--- spec/models/user_skill_spec.rb
+++ spec/models/user_skill_spec.rb
@@ -3,5 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe UserSkill, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { FactoryBot.create(:user, email: 'fake@test.com') }
+  let!(:skill) { FactoryBot.create(:skill) }
+  subject { FactoryBot.create(:user_skill, user: user, skill: skill) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'has an associated user' do
+    expect(subject.user).to eq user
+  end
+
+  it 'has an associated skill' do
+    expect(subject.skill).to eq skill
+  end
 end
