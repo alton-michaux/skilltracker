7580300929097a9719920b7f62a9360ffd1446d8
Alton Michaux <alton.michaux@yahoo.com>
2023-07-02 23:45:18 -0400

    added some placeholder text and removed text from the rails view

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -3,7 +3,7 @@ import React from "react"
 const Home = () => {
   return (
     <div className="text-center">
-      <p>This text is inside the react component</p>
+      <p>Welcome to SkillTracker</p>
     </div>
   );
 }
