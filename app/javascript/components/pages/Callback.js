51ffb0777533d41ad660b649254da38e63c906d5
Alton Michaux <alton.michaux@yahoo.com>
2023-07-29 16:51:21 -0400

    structuring data for the frontend

--- app/javascript/components/pages/Callback.js
+++ app/javascript/components/pages/Callback.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from 'prop-types';
 
 const Callback = ({ client }) => {
+  console.log("🚀 ~ file: Callback.js:6 ~ Callback ~ client:", client)
   return (
     <div className="text-center">Jira Authentication Successful</div>
   )
--- app/javascript/components/pages/UserProfile.js
+++ app/javascript/components/pages/UserProfile.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const UserProfile = () => {
+const UserProfile = ({ user }) => {
   const { id, name } = useParams();
   return (
     <>
@@ -11,10 +11,29 @@ const UserProfile = () => {
       </p>
       <div className="text-center main-div">
         <div className="secondary-div">
-          <p>Your Skills</p>
+          <h2>Your Skills</h2>
+          {
+            user?.skills?.map((data) => {
+              return (
+                <div id={data.skill.id}>
+                  <h4>{data.skill.name}</h4>
+                  <p>{data.skill.description}</p>
+                </div>
+              )
+            })}
         </div>
         <div className="secondary-div">
-          <p>Your Work</p>
+          <h2>Your Work</h2>
+          {
+            user?.tickets?.map((ticket) => {
+              return (
+                <div id={ticket.id}>
+                  <h4>{ticket.title}</h4>
+                  <p>{ticket.status}</p>
+                  <p>{ticket.description}</p>
+                </div>
+              )
+            })}
         </div>
       </div>
     </>
@@ -27,6 +46,7 @@ UserProfile.defaultProps = {
 }
 
 UserProfile.propTypes = {
+  user: PropTypes.object.isRequired,
   id: PropTypes.number,
   name: PropTypes.string
 }
