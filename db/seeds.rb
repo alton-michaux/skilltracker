3a924f901971c3e7c1838ef0ef6d88bc8f712583
alton-michaux <alton.michaux@yahoo.com>
2023-07-29 20:43:51 -0400

    satisfying rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-user1 = User.create(email: "admin@gmail.com", password: "password", first_name: "Tremont", last_name: "Williams")
+user1 = User.create(email: 'admin@gmail.com', password: 'password', first_name: 'Tremont', last_name: 'Williams')
 
 puts "#{User.count} users created"
 
