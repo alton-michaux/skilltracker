c80b80c03f3dfb294202025ddc5e31043f8e5fb1
Alton Michaux <alton.michaux@yahoo.com>
2023-09-28 22:12:43 -0400

    added some formatting to ticket component, looks good for now

--- app/javascript/components/comps/Tickets.js
+++ app/javascript/components/comps/Tickets.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import { useAppContext } from '../AppContext'
+import { truncate } from '../utils/local/app'
 
 const TicketComponent = () => {
   const { state } = useAppContext()
   return (
-    <ul> {
+    <ul className="ticket-div"> {
       Object.keys(state.tickets).length > 0 &&
       state.tickets.map((ticket) => {
         return (
           <li key={ticket.id}>
-            <div className="ticket-div">
-              <div className="card">
-                <div className="card-body">
-                  <img src={ticket.reporter_avatar} className="card-img-top reporter-avatar" alt="Issue reporter avatar" />
-                  <h5 className="card-title">{ticket.title}</h5>
-                  <h7 className="card-text">{ticket.status}</h7>
-                  <p className="card-text">{ticket.description}</p>
-                </div>
+            <div className="card">
+              <div className="card-body">
+                <img src={ticket.reporter_avatar} className="card-img-top reporter-avatar" alt="Issue reporter avatar" />
+                <h5 className="card-title">{ticket.title}</h5>
+                <h7 className="card-title">{ticket.status}</h7>
+                <p className="card-text">{truncate(ticket.description)}</p>
               </div>
             </div>
           </li>
